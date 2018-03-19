df9c219406b86d55966b2a3f87d7b592316a0f71
Daniel Ham <danieljham@gmail.com>
2018-03-19 13:44:40 -0700

    add life and flyind discs

---
+++ src/components/FlyingObject.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FlyingObjectBase from './FlyingObjectBase';
+import FlyingObjectTop from './FlyingObjectTop';
+
+const FlyingObject = props => {
+  return (
+    <g>
+      <FlyingObjectBase position={props.position} />
+      <FlyingObjectTop position={props.position} />
+    </g>
+  );
+};
+
+FlyingObject.propTypes = {
+  position: PropTypes.shape({
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired
+  }).isRequired,
+};
+
+
+export default FlyingObject;
