eeebec667da52a085f4557baddfc49f657755309
Daniel Ham <danieljham@gmail.com>
2018-03-18 23:32:24 -0700

    add cannonball and score

--- src/components/CannonBall.jsx
+++ src/components/CannonBall.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CannonBall = () => {
+const CannonBall = (props) => {
+  const ballStyle = {
+    fill: '#777',
+    stroke: '#444',
+    strokeWidth: '2px',
+  };
   return (
-    <div>
-      
-    </div>
+    <ellipse
+      style={ballStyle}
+      cx={props.position.x}
+      cy={props.position.y}
+      rx="16"
+      ry="16"
+    />  
   );
-import PropTypes from 'prop-types';};
+};
 
+CannonBall.propTypes = {
+  position: PropTypes.shape({
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired
+  }).isRequired,
+}
 
 export default CannonBall;
