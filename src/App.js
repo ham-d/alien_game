a6f0b076a5b930934a1d1acf3ac67285432c5791
Daniel Ham <danieljham@gmail.com>
2018-03-22 18:18:56 -0700

    created leaderboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import * as Auth0 from 'auth0-web';
 
 import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
+import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from './utils/config';
 
+Auth0.configure({
+  domain: AUTH0_DOMAIN,
+  clientID: AUTH0_CLIENT_ID,
+  redirectUri: 'http://localhost:3000/',
+  responseType: 'token id_token',
+  scope: 'openid profile manage:points',
+});
 
 class App extends Component {
   componentDidMount() {
     const _this = this;
+
+    Auth0.handleAuthCallback();
+
+    Auth0.subscribe((auth) => {
+      console.log(auth);
+    });
 
     setInterval(() => {
       _this.props.moveObjects(_this.canvasMousePosition)
